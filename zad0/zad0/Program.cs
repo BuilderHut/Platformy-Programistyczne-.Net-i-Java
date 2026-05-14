3a3b36972935b1e0e5661a3477aa4dd8b04182d2
Adi28 <adi28g@gmail.com>
2026-05-14 20:57:42 +0200

    Add zad0

---
+++ zad0/zad0/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace zad0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz f = new FizzBuzz();
+            f.Print();
+        }
+    }
+
+    class FizzBuzz
+    {
+        private int maxnumber;
+
+        public FizzBuzz()
+        {
+            maxnumber = 20;
+        }
+
+        public void Print()
+        {
+            for (int i = 1; i <= maxnumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
